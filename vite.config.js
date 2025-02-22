8047ef3de8b369766e95e57be7b1a94cc20ceb24
DESKTOP-IBAEHR8 <soodeh.arvin@gmail.com>
2025-02-22 10:52:43 +0330

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['react-textfit'],  // Externalize react-textfit to avoid bundling issues
+    },
+  },
+});
