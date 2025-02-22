790c0dec615a6811af246141e9844691508d40b4
DESKTOP-IBAEHR8 <soodeh.arvin@gmail.com>
2025-02-22 11:21:44 +0330

    fix

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
 import { CalcContext } from '../context/CalcContext'
-import { Textfit } from 'react-textfit';
+
 function Screen() {
     const {calc} = useContext(CalcContext);
   return (
-    <Textfit className='screen' max={70} mode='single'>{calc.num ? calc.num : calc.res}</Textfit>
+    <div className="responsive-text">{calc.num ? calc.num : calc.res}</div>
   )
 }
 
